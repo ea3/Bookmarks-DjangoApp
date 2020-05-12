f50b080d2cbf4cce240df887de816efdc50a3a6c
Emilio Araos <ea3dev@gmail.com>
2020-05-12 07:36:01 -0600

    update dashboard to show bookmarks

--- images/models.py
+++ images/models.py
@@ -8,7 +8,7 @@ class Image(models.Model):
                              related_name='images_created',
                              on_delete=models.CASCADE)
 
-    title = models.URLField()
+    title = models.CharField(max_length=20)
     slug = models.SlugField(max_length=200,
                             blank=True)
     url = models.URLField()
